27d34674297355e9994f63b04aaf80ed84f9b7b4
Creepzzz <60852111+Creepzzz@users.noreply.github.com>
2020-04-24 12:54:02 +0200

    Update ControllerTest.java

--- Seminar3/seminar3/AmazingPos/src/se/kth/iv1350/amazingpos/controller/ControllerTest.java
+++ Seminar3/seminar3/AmazingPos/src/se/kth/iv1350/amazingpos/controller/ControllerTest.java
@@ -2,3 +2,29 @@ package se.kth.iv1350.amazingpos.controller;
 
 import se.kth.iv1350.amazingpos.model.*;
 import se.kth.iv1350.amazingpos.intergation.*;
+
+public class ControllerTest{
+  private Controller ctrl;
+  
+  @Before
+  public void setUp(){
+  ctrl = new Controller(new SystemCreator(), new CatalogCreator(), new Printer());
+  }
+  
+  @After
+  public void tearDown(){
+  ctrl = null;
+  }
+  
+  @Test
+  public void registerItem(){
+  }
+  
+  @Test
+  public void checkItem(){
+  }
+  
+  @Test
+  public void paid(){
+  }
+}
